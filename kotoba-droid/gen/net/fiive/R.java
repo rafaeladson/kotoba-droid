a51c322990ac4b77079ba7a7429ecb0109da8abd
Rafael Adson <rafael@rafaeladson.com>
2011-08-09 21:56:07 -0300

    Melhorando a aparência do aplicativo.

--- kotoba-droid/gen/net/fiive/R.java
+++ kotoba-droid/gen/net/fiive/R.java
@@ -11,16 +11,16 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
-        public static final int linearLayout1=0x7f050000;
-        public static final int linearLayout2=0x7f050002;
-        public static final int linearLayout3=0x7f050004;
-        public static final int nextWordButton=0x7f050005;
-        public static final int showTranslationButton=0x7f050006;
-        public static final int translationLabel=0x7f050003;
-        public static final int valueLabel=0x7f050001;
+        public static final int RelativeLayout1=0x7f060000;
+        public static final int linearLayout3=0x7f060003;
+        public static final int nextWordButton=0x7f060004;
+        public static final int showTranslationButton=0x7f060005;
+        public static final int translationLabel=0x7f060002;
+        public static final int valueLabel=0x7f060001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -30,5 +30,10 @@ public final class R {
         public static final int hello=0x7f040000;
         public static final int next_word=0x7f040002;
         public static final int show_translation=0x7f040003;
+        public static final int translation_asterisks=0x7f040005;
+        public static final int waiting_for_word=0x7f040004;
+    }
+    public static final class style {
+        public static final int LightThemeSelector=0x7f050000;
     }
 }
