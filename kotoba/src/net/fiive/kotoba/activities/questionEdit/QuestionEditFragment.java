b82a310880f18fb395025951579c3b4d8dd5f6fa
rafael.adson <rafael@fiive.net>
2012-02-19 04:59:08 -0200

    Esqueleto da versão 1.0 da aplicação pronto
    O esqueleto tá pronto. Falta só agora:
    1) testar esse esqueleto.
    2) implementar a lógica da aplicação.

--- kotoba/src/net/fiive/kotoba/activities/questionEdit/QuestionEditFragment.java
+++ kotoba/src/net/fiive/kotoba/activities/questionEdit/QuestionEditFragment.java
@@ -2,21 +2,25 @@ package net.fiive.kotoba.activities.questionEdit;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.view.*;
 import net.fiive.kotoba.R;
 
 
 public class QuestionEditFragment extends Fragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		this.setHasOptionsMenu(true);
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		super.onCreateView(inflater, container, savedInstanceState);
-		View editQuestionView = inflater.inflate(R.layout.question_edit, container);
-		return editQuestionView;
+		return inflater.inflate(R.layout.question_edit, container);
+	}
+
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.question_edit, menu);
+		super.onCreateOptionsMenu(menu, inflater);
 	}
 }
--- kotoba/src/net/fiive/kotoba/activities/questionList/QuestionListFragment.java
+++ kotoba/src/net/fiive/kotoba/activities/questionList/QuestionListFragment.java
@@ -3,9 +3,12 @@ package net.fiive.kotoba.activities.questionList;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.ListView;
 import com.google.common.base.Preconditions;
+import net.fiive.kotoba.R;
 import net.fiive.kotoba.activities.questionEdit.QuestionEditActivity;
 import net.fiive.kotoba.dao.QuestionDAO;
 import net.fiive.kotoba.domain.Question;
@@ -17,9 +20,8 @@ public class QuestionListFragment extends ListFragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-
 		this.setListAdapter(new QuestionListAdapter(this.getActivity().getApplicationContext(), questionDAO.findAll()));
+		this.setHasOptionsMenu(true);
 	}
 
 	@Override
@@ -32,6 +34,11 @@ public class QuestionListFragment extends ListFragment {
 		Intent editQuestionIntent = new Intent(QuestionEditActivity.EDIT_QUESTION_ACTION);
 		editQuestionIntent.putExtra("question", questionFromRepository);
 		startActivity(editQuestionIntent);
+	}
 
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.question_list, menu);
+		super.onCreateOptionsMenu(menu, inflater);
 	}
 }
